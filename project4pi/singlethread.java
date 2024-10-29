20b8d8576cc3761fa3db1f0965da6411ed6c3124
bsucs <122378640+bsucs@users.noreply.github.com>
2024-10-29 08:26:17 -0400

    itial

---
+++ project4pi/singlethread.java
@@ -0,0 +1,31 @@
+
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class singlethread {
+private static final long totalPoints =1000000000000000000L;
+static int Pointsincirlce=0;
+public static void main(String[] args) {
+    Instant start= Instant.now();
+  
+    for(long i=0; i<totalPoints;i++){
+      double x = ThreadLocalRandom.current().nextDouble(0,2);
+      double y = ThreadLocalRandom.current().nextDouble(0,2);
+      double distance = Math.sqrt((x-1)*(x-1)+(y-1)*(y-1));
+      if(distance<=1){
+       
+        Pointsincirlce++;
+    }
+    
+}
+
+double pi = Pointsincirlce/(double)totalPoints*4;
+Instant finish = Instant.now();
+long TimeElapsed = Duration.between(start, finish).toMillis();
+System.out.println("Pi="+pi);
+System.out.println("Time Elapsed"+TimeElapsed);
+}
+
+}
