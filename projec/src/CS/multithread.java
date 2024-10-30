4c2b29d0897dc8e0d9aff096947ef3b14613e631
bsucs <122378640+bsucs@users.noreply.github.com>
2024-10-30 13:11:22 -0400

    df

--- projec/src/CS/multithread.java
+++ projec/src/CS/multithread.java
@@ -6,7 +6,7 @@ import java.time.Duration;
 import java.time.Instant;
 public class multithread{
 
-    private static final long TOTAL_POINTS=100_000_000_000_000L;
+    private static final long TOTAL_POINTS=100_000_000L;
     private static final int THREAD_COUNT=20;
     static class PiCalc implements Callable<Long>{
         private final long points;
